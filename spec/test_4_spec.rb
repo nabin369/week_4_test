af89e166374a22bed2dc290043b839540342d644
nabin369 <nabinrai369@gmail.com>
2014-01-31 16:02:51 +0000

    updated

--- spec/test_4_spec.rb
+++ spec/test_4_spec.rb
@@ -3,27 +3,27 @@ require_relative '../lib/test_4'
 describe 'test' do 
 
 	it 'sum of three elements of enum, specified by a block' do
-		arrayReloded = ArrayReloded.new([1,2,3])
+		arrayReloded = ArrayReloded.new([2,3,4])
 
-		expect(arrayReloded.injection{|x, y| x + y}).to eq(6)
+		expect(arrayReloded.injection{|x, y| x + y}).to eq(9)
 	end
 	
 	it 'sum of three elements of enum with argument, specified by a block' do
-		arrayReloded = ArrayReloded.new([1,2,3])
+		arrayReloded = ArrayReloded.new([2,3,5])
 
-		expect(arrayReloded.injection(2){|x, y| x + y}).to eq(8)
+		expect(arrayReloded.injection(2){|x, y| x + y}).to eq(12)
 	end
 
-	it 'returns subtraction of 1 and 4 passed 5 as argument = , specified by a block' do
-		arrayReloded = ArrayReloded.new([1,2,3])
+	it 'returns subtraction of 2,3,4 passed 5 as argument = -4, specified by a block' do
+		arrayReloded = ArrayReloded.new([2,3,4])
 
-		expect(arrayReloded.injection(5){|x,y| x - y}).to eq(-1)
+		expect(arrayReloded.injection(5){|x,y| x - y}).to eq(-4)
 	end
 
-	it 'returns subtraction of 1,2 and 3, specified by a block' do
-		arrayReloded = ArrayReloded.new([1,2,3])
+	it 'returns subtraction of 2,3 and 4 = -5, specified by a block' do
+		arrayReloded = ArrayReloded.new([2,3,4])
 
-		expect(arrayReloded.injection{|x, y| x -y}).to eq(-4)
+		expect(arrayReloded.injection{|x, y| x -y}).to eq(-5)
 	end
 
 	it 'multiplication of 1,2,3 = 6, specified by a block' do
@@ -32,10 +32,10 @@ describe 'test' do
 		expect(arrayReloded.injection{|x, y| x *y}).to eq(6)
 	end
 
-	it 'multiplication of 1,2,3 with 6 as argument, specified by a block' do
-		arrayReloded = ArrayReloded.new([1,2,3])
+	it 'multiplication of 2,3,4 with 6 as argument = , specified by a block' do
+		arrayReloded = ArrayReloded.new([2,3,4])
 
-		expect(arrayReloded.injection(6){|x, y| x *y}).to eq(36)
+		expect(arrayReloded.injection(2){|x, y| x *y}).to eq(48)
 	end
 
 end
